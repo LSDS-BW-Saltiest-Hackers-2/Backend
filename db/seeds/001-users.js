5f5b065a2833e951a72e49aa020377dea07c5a9f
miriam-sullivan <maljayd42@gmail.com>
2020-05-26 16:04:08 -0500

    made slight changes to seeded data

--- db/seeds/001-users.js
+++ db/seeds/001-users.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("users").insert([
-        { id: 1, username: "test", password: "1", comment_id: 1 },
-        { id: 2, username: "test2", password: "2", comment_id: 2 },
-        { id: 3, username: "test3", password: "3", comment_id: 3 },
+        { id: 1, username: "test", password: "1" },
+        { id: 2, username: "test2", password: "2" },
+        { id: 3, username: "test3", password: "3" },
       ]);
     });
 };

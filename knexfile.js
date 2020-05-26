7e876336777b224b1475405709e55426564db721
miriam-sullivan <maljayd42@gmail.com>
2020-05-26 15:09:46 -0500

    lets try this

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ const pgConnection =
 module.exports = {
   development: {
     client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./db/usersAndC.db3",
     },
@@ -14,7 +15,6 @@ module.exports = {
         // runs after a connection is made to the sqlite engine
         conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
-      useNullAsDefault: true,
     },
     migrations: {
       directory: "./db/migrations",

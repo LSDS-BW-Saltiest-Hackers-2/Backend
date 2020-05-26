bda8e0a37a9c1c374bace251bb4a984acf6171f6
miriam-sullivan <maljayd42@gmail.com>
2020-05-26 15:16:59 -0500

    try this again

--- usersAndC/usersAndC-router.js
+++ usersAndC/usersAndC-router.js
@@ -1,47 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 const server = express();
-const multer = require("multer");
-const upload = multer({ dest: "/public" });
-const Model = require("./usersAndC-model.js");
 
 server.use(express.json());
 server.use(isValid);
-// server.use(bodyParser.json());
-// server.use(bodyParser.urlencoded({ extended: true }));
-// server.use(morgan("dev"));
-// server.use(express.static(__dirname + "/public"));
-
-// const storage = multer.diskStorage({
-//   destination: "../images",
-//   filename: function (req, file, callback) {
-//     crypto.pseudoRandomBytes(16, function (err, raw) {
-//       if (error) return callback(error);
-//       callback(null, raw.toString("hex") + path.extname(file.originalname));
-//     });
-//   },
-// });
-
-// server.post(
-//   "/",
-//   upload.single("avatar", (req, res) => {
-//     if (!req.file) {
-//       console.log("no file recieved");
-//       res.status(404).json({ message: "no image/file was found or recieved" });
-//       return res.send({ success: false });
-//     } else {
-//       console.log("file recieved");
-//       const host = req.host;
-//       const filePath = req.protocol + "://" + host + "/" + req.file.path;
-//       res.status(200).json({ message: "you successfully sent the image/file" });
-//       return res.send({ success: true });
-//     }
-//   })
-// );
-
+//
 function isValid(user) {
   return Boolean(
     user.username && user.password && typeof user.password === "string"
